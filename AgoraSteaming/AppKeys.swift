ca8cf927481c2eb5435cf94bb23006088fff7b19
ATursunbekov <70901661+ATursunbekov@users.noreply.github.com>
2023-04-04 10:28:16 +0600

    Update AppKeys.swift

--- AgoraSteaming/AppKeys.swift
+++ AgoraSteaming/AppKeys.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct AppKeys {
     static let agoraAppId: String = "4c1ba97f72a543269498ee6e5a66bca3"
-    static let agoraToken: String? = "007eJxTYAiz3ZcROnvJBJ/fsfUhN+Mi3SdYH/n5SXHP64gzDgwJMWYKDCbJhkmJluZp5kaJpibGRmaWJpYWqalmqaaJZmZJyYnGiszqKQ2BjAyf/fSYGRkgEMRnYwguKUpNzGVgAAB08B8M"
+    static let agoraToken: String? = "007eJxTYGB0fJPdHl7WF1B9+hKH6xHPF2nG6W/2afDfVLG+/eD7v9cKDCbJhkmJluZp5kaJpibGRmaWJpYWqalmqaaJZmZJyYnGLsu1UxoCGRkUrrswMzJAIIjPxhBcUpSamMvAAADBGiC5"
     static let visionLabApiKey: String = "23c0bccf2fe4467e912100d2e74fccef"
     static let visionLabApiSecret: String = "8429a99c528849fe981b549c882c36d4"
 }
